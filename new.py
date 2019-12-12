759842bdbb2ae1ccc9417792aba13acb25d5c709
Daniel Peretz <daniel.perez079@gmail.com>
2019-12-12 19:42:07 +0200

    Add files via upload

---
+++ new.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+import pickle
+
+CACHE = 'cache'
+POST_VIEWSTATE = '/wEPDwUIMjU3MTQzOTcPZBYGZg8WAh4EVGV4dAU+PCFET0NUWVBFIEhUTUwgUFVCTElDICItLy9XM0MvL0RURCBIVE1MIDQuMCBU \
+                  cmFuc2l0aW9uYWwvL0VOIj5kAgEPZBYMAgEPFgIeB1Zpc2libGVoZAICDxYCHgdjb250ZW50BSLXk9eUINep15zXmdeYINeX15gi \
+                  16Ig16jXl9eV15HXldeqZAIDDxYCHwIFIteT15Qg16nXnNeZ15gg15fXmCLXoiDXqNeX15XXkdeV16pkAgQPFgIfAgUg15vXnCDX \
+                  lNeW15vXldeZ15XXqiDXqdee15XXqNeV16pkAgUPFgQfAmQfAWhkAgYPFgIfAgUi15PXlCDXqdec15nXmCDXl9eYIteiINeo15fX \
+                  ldeR15XXqmQCAg9kFgJmD2QWAgIED2QWAmYPZBYGAgIPZBYCZg8PFgYeCENzc0NsYXNzBQtza2luY29sdHJvbB4EXyFTQgICHwFo \
+                  ZGQCAw9kFgJmDw8WBh8DBQtza2luY29sdHJvbB8ABRfXm9eg15nXodeUINec157Xoteo15vXqh8EAgJkZAIKD2QWAgICD2QWCAIB \
+                  Dw8WAh8BaGRkAgMPDxYCHwFoZGQCBQ9kFgICAg8WAh8BaGQCBw9kFgICAQ9kFgICAQ9kFggCBg9kFgJmD2QWDAICDxYCHgVjbGFz \
+                  cwUKSGVhZGVyQ2VsbGQCBA8WAh8FBQpIZWFkZXJDZWxsZAIGDxYCHwUFCkhlYWRlckNlbGxkAggPFgIfBQUKSGVhZGVyQ2VsbGQC \
+                  Cg8WAh8FBQpIZWFkZXJDZWxsZAIMDxYCHwUFEEhlYWRlckNlbGxCdXR0b25kAgcPEGQQFQAVABQrAwBkZAIMD2QWAmYPZBYeZg9k \
+                  FgICAQ8QZBAVHgvXmSAxINee15HXqATXmSAyBNeZIDME15kgNATXmSA1BNeZIDYE15kgNwTXmSA4BNeZIDkJ15kgMTAgYXNkDdeZ \
+                  15AgMSDXnteR16gG15nXkCAyBteZ15AgMwbXmdeQIDQG15nXkCA1BteZ15AgNgbXmdeQIDcG15nXkCA4BteZ15AgOQfXmdeQIDEw \
+                  C9eZ15AgMTEgYXNkDdeZ15EgMSDXnteR16gG15nXkSAyBteZ15EgMwbXmdeRIDQG15nXkSA1BteZ15EgNgbXmdeRIDcG15nXkSA4 \
+                  BteZ15EgORUeATMBNQE2ATcBOAE5AjEwAjExAjQwAjQ5AjEzAjE0AjE1AjE2AjE3AjE4AjE5AjIwAjQ0AjQ4AjQ2AjIyAjIzAjI0 \
+                  AjI1AjI2AjI3AjI4AjI5AjQxFCsDHmdnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZxYBAhhkAgIPFgQfBQUKSGVhZGVyQ2Vs \
+                  bB8BaGQCAw8WAh8BaGQCBA8WAh8FBQpIZWFkZXJDZWxsZAIGDxYCHwUFCkhlYWRlckNlbGxkAggPFgIfBQUKSGVhZGVyQ2VsbGQC \
+                  Cg8WAh8FBQpIZWFkZXJDZWxsZAIMDxYEHwUFCkhlYWRlckNlbGwfAWhkAg0PFgIfAWhkAg4PFgIfBQUKSGVhZGVyQ2VsbGQCEA8W \
+                  BB8FBQpIZWFkZXJDZWxsHwFoZAIRDxYCHwFoZAISDxYEHwUFCkhlYWRlckNlbGwfAWhkAhMPFgIfAWhkAhQPFgIfBQUQSGVhZGVy \
+                  Q2VsbEJ1dHRvbmQCDw8PFgIfAAU7157XoteV15PXm9efINecOiAwMi4xMi4yMDE5LCDXqdei15Q6IDA2OjQ5LCDXnteh15o6IEEz \
+                  MTEzOTZkZGQrAF1uagsNnzUhswtfflythWXMAw=='
+
+class Lesson (object):
+
+    DAYS = ['', 'ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי']
+
+    def __init__(self, name, teacher, classroom, day, period):
+        """
+        :param name: subject name
+        :param teacher: teacher
+        :param classroom: classroom location / id
+        :param day: day of the week (1=Sunday)
+        :param period: period number
+        """
+        self.name = name
+        self.teacher = teacher
+        self.classroom = classroom
+        self.day = day
+        self.period = period
+
+    def __str__(self):
+        return '%s מלמד/ת %s ביום %s בשעה %s בכיתה %s' % (self.teacher,
+                                                          self.name,
+                                                          self.DAYS[self.day],
+                                                          self.period,
+                                                          self.classroom)
+
+
+class Bot (object):
+
+    def __init__(self):
+        try:
+            with open(CACHE, 'rb') as cachef:
+                self.cache = pickle.load(cachef)
+        except:
+            self.save_cache()
+            with open(CACHE, 'rb') as cachef:
+                self.cache = pickle.load(cachef)
+        # self.cache = {'classes': {num: name}, 'lessons': [Lesson]}
+
+    def get_teacher_schedule(self, teacher, day):
+        return sorted(filter(lambda x: x.day == day and teacher in x.teacher, self.cache['lessons']), key=lambda x: x.period)
+
+    def save_cache(self):  # ! This will send web requests
+        classes = self.fetch_classes()  # {num: name}
+        lessons = list()
+        for classnum in classes.keys():
+            lessons += self.fetch_lessons(classnum)
+        with open(CACHE, 'wb') as cachef:
+            pickle.dump({'classes': classes, 'lessons': lessons}, cachef)
+
+    def fetch_classes(self):
+        """:returns: {classnum: classname}"""
+        soup = BeautifulSoup(self.fetch_page(), 'html.parser')
+        select = soup.find('select', id='dnn_ctr11396_TimeTableView_ClassesList')
+        return {option['value']: option.text for option in select.findChildren('option')}
+
+    def fetch_lessons(self, classnum):
+        soup = BeautifulSoup(self.fetch_page(classnum), 'html.parser')
+        table = soup.find('div', id='dnn_ctr11396_TimeTableView_PlaceHolder').find('table')
+        rows = table.findChildren('tr', recursive=False)
+        lessons = list()
+        for rownum, row in enumerate(rows[1:]):
+            for colnum, col in enumerate(row.findChildren('td', recursive=False)[1:]):
+                for lessondiv in col.findAll('div', {'class': 'TTLesson'}):
+                    lessons.append(Lesson(lessondiv.find('b').text,
+                                          lessondiv.find('br').nextSibling,
+                                          lessondiv.find('b').nextSibling,
+                                          colnum + 1,
+                                          rownum))
+                    print(lessons[-1])
+        return lessons
+        
+        print([[col.findAll('div', {'class': 'TTLesson'}) for col in row.findChildren('td', recursive=False)[1:]]
+                for row in rows[1:]][1][0][0])
+
+    def fetch_page(self, classnum=0):  # ! This will generate web requests
+        r = requests.post('http://deshalit.iscool.co.il/default.aspx',
+                          data={'__EVENTTARGET': 'dnn$ctr11396$TimeTableView$btnChangesTable',
+                                '__VIEWSTATE': POST_VIEWSTATE,
+                                'dnn$ctr11396$TimeTableView$ClassesList': str(classnum)})
+        return r.text
